d2d9d1634312712f74c0cf078cdf93c23b55380f
lincoln <Lincoln_27@hotmail.com>
2018-12-01 23:11:10 -0300

    Modulo3 blocos/ extra love

---
+++ modulo3/self/love.rb
@@ -0,0 +1,21 @@
+
+ 
+ 
+ class William  
+  end
+ 
+ William.class #=> Class
+   puts "Digite o nome da amada:"
+   nome = gets.chomp
+
+ puts "#################################"
+
+ class Class
+   def loud_name
+     "#{name.upcase} \n &"
+   end
+ end
+
+   puts William.loud_name #=> "William &"
+   puts "#{nome.upcase} só love.!!!"
+
